499946d4f7a2176d4d1847b716d7b8c0048da000
Mason Fischer <mason@kissr.co>
2018-03-08 15:46:01 -0500

    Test sending tokens back and forth ✨

--- lib/crypto.ex
+++ lib/crypto.ex
@@ -1,21 +1,32 @@
 defmodule Crypto do
   def keypair do
-    %{public: public, secret: secret} = :enacl.crypto_sign_ed25519_keypair
+    # %{public: public, secret: secret} = :enacl.crypto_sign_ed25519_keypair
+    #
+    # {
+    #   public,
+    #   secret,
+    # }
 
-    {
-      public,
-      secret,
-    }
+    :libsodium_crypto_sign_ed25519.keypair()
   end
 
-  def sign(data, secret_key) do
-    :enacl.sign_detached(data, secret_key)
+  def sign(message, secret_key) do
+    # :enacl.sign_detached(data, secret_key)
+    :libsodium_crypto_sign_ed25519.detached(message, secret_key)
   end
 
-  def valid_signature?(signature, data, public_key) do
-    case :enacl.sign_verify_detached(signature, data, public_key) do
-      {:ok, _data} -> true
-      {:error, _message} -> false
+  def valid_signature?(signature, message, public_key) do
+    # case :enacl.sign_verify_detached(signature, message, public_key) do
+    #   {:ok, _data} -> true
+    #   {:error, _message} -> false
+    # end
+    case :libsodium_crypto_sign_ed25519.verify_detached(signature, message, public_key) do
+      0 -> true
+      1 -> false
     end
+  end
+
+  def public_key_from_private_key(private_key) do
+    :libsodium_crypto_sign_ed25519.sk_to_pk(private_key)
   end
 end
--- test/integration/base_token_test.exs
+++ test/integration/base_token_test.exs
@@ -1,20 +1,23 @@
 defmodule Integration.BaseTokenTest do
   @host "http://localhost:4047/"
   @sender  Base.decode16!("b114ed4c88b61b46ff544e9120164cb5dc49a71157c212f76995bf1d6aecab0e", case: :lower)
-  @private_key Base.decode16!("2a185960faf3ffa84ff8886e8e2e0f8ba0fff4b91adad23108bfef5204390483b114ed4c88b61b46ff544e9120164cb5dc49a71157c212f76995bf1d6aecab0e", case: :lower)
-  @receiver <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2>>
-  @base_token_contract Base.decode16("b114ed4c88b61b46ff544e9120164cb5dc49a71157c212f76995bf1d6aecab0e", case: :lower)
+  @sender_private_key Base.decode16!("2a185960faf3ffa84ff8886e8e2e0f8ba0fff4b91adad23108bfef5204390483b114ed4c88b61b46ff544e9120164cb5dc49a71157c212f76995bf1d6aecab0e", case: :lower)
+  @receiver  Base.decode16!("027da28b6a46ec1124e7c3c33677b71f4ac4eae2485ff8cb33346aac54c11a30", case: :lower)
+  @receiver_private_key Base.decode16!("1e598351b3347ca287da6a77de2ca43fb2f7bd85350d54c870f1333add33443a027da28b6a46ec1124e7c3c33677b71f4ac4eae2485ff8cb33346aac54c11a30", case: :lower)
+  @base_token_contract Base.decode16("93a88d0da2c295543a235e69cf84e9590fa4f7398c8905b2256429fc958f3bdedcb834ae4fdd073f9f3949c78551e8e1c842e408be672e70cd3d74fc6c1601b5", case: :lower)
 
   use ExUnit.Case
 
   test "send some tokens" do
     call(%{
+      private_key: @sender_private_key,
       nonce: 0,
       method: :constructor,
       params: [100],
     })
 
     {:ok, response} = call(%{
+      private_key: @sender_private_key,
       nonce: 2,
       method: :balance_of,
       params: [@sender, 100],
@@ -23,23 +26,40 @@ defmodule Integration.BaseTokenTest do
     assert Cbor.decode(response.body) == 100
 
     call(%{
+      private_key: @sender_private_key,
       nonce: 1,
       method: :transfer,
-      params: [@receiver, 3],
+      params: [@receiver, 50],
     })
 
     {:ok, response} = call(%{
+      private_key: @sender_private_key,
       nonce: 2,
       method: :balance_of,
-      params: [@sender, 3],
+      params: [@sender],
     })
 
-    assert Cbor.decode(response.body) == 97
+    assert Cbor.decode(response.body) == 50
 
+    call(%{
+      private_key: @receiver_private_key,
+      nonce: 0,
+      method: :transfer,
+      params: [@sender, 25],
+    })
 
+    {:ok, response} = call(%{
+      private_key: @sender_private_key,
+      nonce: 2,
+      method: :balance_of,
+      params: [@sender],
+    })
+
+    assert Cbor.decode(response.body) == 75
   end
 
   def call(%{
+    private_key: private_key,
     nonce: nonce,
     method: method,
     params: params,
@@ -49,8 +69,10 @@ defmodule Integration.BaseTokenTest do
       params: params,
     })
 
-    message = @sender <> <<nonce::size(32)>> <> rpc
-    signature = Crypto.sign(message, @private_key)
+    public_key =  Crypto.public_key_from_private_key(private_key)
+    message = public_key <> <<nonce::size(32)>> <> rpc
+    signature = Crypto.sign(message, private_key)
+    IO.inspect Crypto.valid_signature?(signature, message, public_key)
 
     HTTPoison.post(
       @host,
