ab59fc30b0f8fddd323ab0559586ea04e587e7f2
Mason Fischer <mason@kissr.co>
2018-11-07 11:22:15 -0600

    Refactor Ethereum contracts

--- test/support/utils.ex
+++ test/support/utils.ex
@@ -3,6 +3,7 @@ defmodule Test.Utils do
   @default_gas_price 20_000_000_000
   require Integer
   import Binary
+  alias EthereumContracts.{EllipticoinStakingContract, TestnetToken}
 
   def set_balances(balances) do
     token_balance_address =
@@ -29,131 +30,18 @@ defmodule Test.Utils do
     random_seed = <<0::256>>
     ExW3.Contract.start_link()
     address = Enum.at(ExW3.accounts(), 0)
-    {:ok, token_contract_address} = deploy(:TestnetToken, ["[testnet] DAI Token", "DAI", 3])
+    {:ok, token_contract_address} = EthereumContracts.deploy(TestnetToken, ["[testnet] DAI Token", "DAI", 3])
 
     {:ok, staking_contract_address} =
-      deploy(:EllipitcoinStakingContract, [ExW3.to_decimal(token_contract_address), random_seed])
+      EthereumContracts.deploy(EllipticoinStakingContract, [ExW3.to_decimal(token_contract_address), random_seed])
 
     Enum.each(Enum.take(ExW3.accounts(), 3), fn account ->
-      mint(account, amount)
-      approve(staking_contract_address, amount, account)
-      deposit(amount, address)
+      TestnetToken.mint(account, amount)
+      TestnetToken.approve(staking_contract_address, amount, account)
+      EllipticoinStakingContract.deposit(amount, address)
     end)
 
-    {:ok, winner} = winner()
-  end
-
-  defp mint(address, amount),
-    do:
-      ExW3.Contract.send(:TestnetToken, :mint, [ExW3.to_decimal(address), amount], %{
-        from: address,
-        gas: 6_721_975
-      })
-
-  defp approve(address, amount, from),
-    do:
-      ExW3.Contract.send(:TestnetToken, :approve, [ExW3.to_decimal(address), amount], %{
-        from: from,
-        gas: 6_721_975
-      })
-
-  defp deposit(amount, address),
-    do:
-      ExW3.Contract.send(:EllipitcoinStakingContract, :deposit, [amount], %{
-        from: address,
-        gas: 6_721_975
-      })
-
-  defp winner(),
-    do: ExW3.Contract.call(:EllipitcoinStakingContract, :winner)
-
-  def deploy(contract_name, args \\ []) do
-    abi_file_name = Path.join(["test", "support", "#{contract_name}.abi"])
-    bin_file_name = Path.join(["test", "support", "#{contract_name}.hex"])
-    abi = ExW3.load_abi(abi_file_name)
-    bin = ExW3.load_bin(bin_file_name)
-    ExW3.Contract.register(contract_name, abi: abi)
-
-    {:ok, address, tx_hash} =
-      ExW3.Contract.deploy(contract_name,
-        bin: bin,
-        args: args,
-        options: %{
-          gas: 6_721_975,
-          from: ExW3.accounts() |> Enum.at(0)
-        }
-      )
-
-    ExW3.Contract.at(contract_name, address)
-    {:ok, address}
-  end
-
-  # def deploy(contract_file_name) do
-  #   auto_mine = Application.get_env(:blacksmith, :ethereumex_auto_mine)
-  #   contract_binary = File.read!(Path.join(test_support_dir(), contract_file_name))
-  #
-  #   private_key = Application.get_env(:blacksmith, :private_key)
-  #   address = private_key_to_address(private_key)
-  #
-  #   transaction_count =
-  #     Ethereumex.WebSocketClient.eth_get_transaction_count("0x" <> Base.encode16(address))
-  #     |> elem(1)
-  #     |> parse_hex()
-  #     |> :binary.decode_unsigned()
-  #
-  #   transaction_data =
-  #     %Blockchain.Transaction{
-  #       data: contract_binary,
-  #       gas_limit: @default_gas_limit,
-  #       gas_price: @default_gas_price,
-  #       nonce: transaction_count,
-  #       value: 0
-  #     }
-  #     |> Blockchain.Transaction.Signature.sign_transaction(private_key)
-  #     |> Blockchain.Transaction.serialize()
-  #     |> ExRLP.encode()
-  #     |> Base.encode16()
-  #
-  #   {:ok, transaction_hash} =
-  #     Ethereumex.WebSocketClient.eth_send_raw_transaction("0x" <> transaction_data)
-  #
-  #   Ethereumex.WebSocketClient.eth_subscribe("newHeads")
-  #   if auto_mine do
-  #     Ethereumex.WebSocketClient.request("evm_mine", [], [])
-  #   end
-  #   {:ok, receipt} = wait_for_receipt(transaction_hash)
-  #
-  #   {:ok, receipt["contractAddress"]}
-  # end
-
-  def wait_for_receipt(transaction_hash) do
-    Ethereumex.WebSocketClient.request("evm_mine", [], [])
-
-    receive do
-      _ ->
-        nil
-    end
-
-    case Ethereumex.WebSocketClient.eth_get_transaction_receipt(transaction_hash) do
-      {:ok, nil} ->
-        wait_for_receipt(transaction_hash)
-
-      receipt ->
-        receipt
-    end
-  end
-
-  def private_key_to_address(private_key) do
-    private_key_to_public_key(private_key)
-    |> ExthCrypto.Key.der_to_raw()
-    |> ExthCrypto.Hash.Keccak.kec()
-    |> EVM.Helpers.take_n_last_bytes(20)
-  end
-
-  def private_key_to_public_key(private_key) do
-    private_key
-    |> ExthCrypto.Signature.get_public_key()
-    |> elem(1)
+    {:ok, winner} = EllipticoinStakingContract.winner()
   end
 
   def read_test_wasm(file_name) do
