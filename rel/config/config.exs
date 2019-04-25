2f1e47fa29135fee1afc56c33c1c46183aa94321
Mason Fischer <mason@kissr.co>
2019-04-25 14:38:58 +0200

    Update production config

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,14 +1,13 @@
 use Mix.Config
 
 config :logger, level: :info
-config :node, transaction_processing_time: 15000
-config :node, mining_target_time: 1000
-config :node, base_contracts_path: "./priv/base_contracts"
+config :node, transaction_processing_time: 1000
+config :node, mining_target_time: 15000
+config :node, base_contracts_path: "lib/node-0.1.0/priv/base_contracts/"
 config :node, port: 4460
 config :node, enable_miner: true
 config :node, private_key: "FbJ84KTznL4ME5InsxJalt5Asv5tVTRJwGnkZTMXMLe9Ayfcm9LcBMhO15D6A5h+5VKfZu64Af7h7w1j8K+3AA==" |> Base.decode64!()
 config :node, P2P.Transport.Noise,
-  private_key: "FbJ84KTznL4ME5InsxJalt5Asv5tVTRJwGnkZTMXMLe9Ayfcm9LcBMhO15D6A5h+5VKfZu64Af7h7w1j8K+3AA==" |> Base.decode64!(),
   port: 4047,
   bootnodes:  File.read!("lib/node-0.1.0/priv/bootnodes.txt")
    |> String.split("\n", trim: true)
@@ -19,6 +18,6 @@ config :node, Node.Repo,
   username: "ellipticoin",
   password: "",
   database: "ellipticoin",
-  hostname: "localhost",
+  socket_dir: "/var/run/postgresql",
   pool_size: 15,
   loggers: []
