7dc3981e984491eea72001b1711f92b879d84207
Mason Fischer <mason@kissr.co>
2019-04-29 11:22:40 +0200

    Add an empty test for block reorganizations

--- test/transaction_processor_test.exs
+++ test/transaction_processor_test.exs
@@ -76,6 +76,47 @@ defmodule TransactionProcessorTest do
            }) == {:error, [{:return_code_mismatch, 0, 2}]}
   end
 
+  @doc """
+  If a new block comes in with a higher total difficulty than the current
+  highest block we need to reverse the results of transaction in orphaned blocks.
+
+  Take for example this case:
+
+  Start with a chain:
+
+  A --- B --- C
+
+  Where total block difficulty increases in alphabetical order.
+
+
+  Now imagine block F comes in with a higher total difficulty than C.
+  The chain now looks like this:
+  
+
+         - B --- C
+        /
+       /
+  A ----
+       \
+        \
+         - D --- E ---F
+
+  B and C are now orphaned blocks.
+  Therefore all transactions that were included in B and C that aren't
+  included in D and E need to be reverted.
+
+  To do this first we revert the state back to before the fork:
+
+  A
+
+  Then we apply the new blocks:
+
+
+  A --- D --- E --- F
+  """
+  test ".process_transaction reverts state changes of transactions that aren't on this chain" do
+  end
+
   def insert_test_contract(contract_name) do
     %Contract{
       address: <<0::256>>,
