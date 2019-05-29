43347199339b9618e10872ce68309f432225051e
Mason Fischer <mason@kissr.co>
2019-05-29 10:55:06 -0400

    Add memory test

--- test/transaction_processor/vm_test.exs
+++ test/transaction_processor/vm_test.exs
@@ -1,4 +1,5 @@
 defmodule TransactionProcessor.VMTest do
+  alias Ellipticoind.Memory
   import Test.Utils
   use ExUnit.Case
 
@@ -32,5 +33,17 @@ defmodule TransactionProcessor.VMTest do
                number: 1
              }
            ) == {:ok, 1}
+  end
+
+  test "state.wasm - memory" do
+    insert_test_contract(:state)
+
+    run_transaction(
+             %{
+               contract_name: :state,
+               function: :set_memory,
+               arguments: [:test]
+             })
+    assert Memory.get_value(<<0::256>>, :state, "value") == :test
   end
 end
