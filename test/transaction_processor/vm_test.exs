158c15e8a2acd0211152ee8d9a0b58991165aa32
Mason Fischer <mason@kissr.co>
2019-06-03 10:06:54 -0400

    Removing var/storage from git history

--- test/transaction_processor/vm_test.exs
+++ test/transaction_processor/vm_test.exs
@@ -47,6 +47,7 @@ defmodule TransactionProcessor.VMTest do
     assert Memory.get_value(<<0::256>>, :state, "value") == :test
   end
 
+  @tag :skip
   test "state.wasm - storage" do
     insert_test_contract(:state)
 
