3301191dca34cba661c1da67b5c4e49c16e27c38
Mason Fischer <mason@kissr.co>
2019-06-18 07:50:47 -0400

    Use Ed25519-dalek
    Sodiumoxide wasn't building on Ubuntu

--- native/ed25519/src/lib.rs
+++ native/ed25519/src/lib.rs
@@ -1,6 +1,12 @@
+extern crate rand;
+extern crate ed25519_dalek;
+
+use sha2::Sha512;
+use rand::rngs::OsRng;
+use ed25519_dalek::{Keypair, PublicKey};
+use ed25519_dalek::Signature;
 use rustler::types::{Binary, OwnedBinary};
 use rustler::{Encoder, Env, Error, Term};
-use sodiumoxide::crypto::sign::ed25519;
 use std::io::Write;
 
 mod atoms {
@@ -21,42 +27,41 @@ rustler::rustler_export_nifs!(
 );
 
 fn keypair<'a>(env: Env<'a>, _args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let (pk, sk) = ed25519::gen_keypair();
+    let mut csprng: OsRng = OsRng::new().unwrap();
+let keypair: Keypair = Keypair::generate::<Sha512, _>(&mut csprng);
+
     Ok((
-        to_binary(pk.as_ref().to_vec()).release(env),
-        to_binary(sk.as_ref().to_vec()).release(env),
+        to_binary(keypair.public.as_bytes().to_vec()).release(env),
+        to_binary(keypair.to_bytes().to_vec()).release(env),
     )
         .encode(env))
 }
 
 fn sign<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
     let message: Binary = args[0].decode()?;
-    let private_key: Binary = args[1].decode()?;
+    let keypair_binary: Binary = args[1].decode()?;
+    let keypair = Keypair::from_bytes(keypair_binary.as_ref()).unwrap();
+    let signature = keypair.sign::<Sha512>(message.as_ref());
 
-    let signature: Vec<u8> = ed25519::sign(
-        message.as_slice(),
-        &ed25519::SecretKey::from_slice(private_key.as_slice()).unwrap(),
-    );
-    Ok((to_binary(signature[..signature.len() - message.len()].to_vec()).release(env)).encode(env))
+    Ok((to_binary(signature.to_bytes().to_vec()).release(env)).encode(env))
 }
 fn valid_signature<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let signature: Binary = args[0].decode()?;
+    let signature_binary: Binary = args[0].decode()?;
     let message: Binary = args[1].decode()?;
     let public_key: Binary = args[2].decode()?;
-    &ed25519::Signature::from_slice(signature.as_slice()).unwrap();
 
-    let valid = ed25519::verify_detached(
-        &ed25519::Signature::from_slice(signature.as_slice()).unwrap(),
-        message.as_slice(),
-        &ed25519::PublicKey::from_slice(public_key.as_slice()).unwrap(),
-    );
+    let signature = Signature::from_bytes(signature_binary.as_ref()).unwrap();
+    let public_key = PublicKey::from_bytes(public_key.as_ref()).unwrap();
+    let valid = public_key.verify::<Sha512>(message.as_ref(), &signature).is_ok();
+
     Ok((valid).encode(env))
 }
 
 fn private_key_to_public_key<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let private_key: Binary = args[0].decode()?;
+    let keypair_binary: Binary = args[0].decode()?;
+    let keypair = Keypair::from_bytes(keypair_binary.as_ref()).unwrap();
 
-    Ok((to_binary(private_key[ed25519::PUBLICKEYBYTES..].to_vec()).release(env)).encode(env))
+    Ok((to_binary(keypair.public.as_bytes().to_vec()).release(env)).encode(env))
 }
 
 fn to_binary(vec: Vec<u8>) -> OwnedBinary {
